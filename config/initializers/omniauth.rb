8a6958fc58e53e63e3600358f6564b01a3e21248
Fadil Sutomo <fadil@warmgreet.com>
2014-04-19 17:24:15 +0700

    Add FB login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '514346035345041', '774b6afa5b332fb071c409a4ffdb5f7c'
 
-  provider :twitter, ENV['caB1N6FvkE0nhaemotOVYg'], ENV['HhDaZpjB5SuXW4CeJPMD72zAh6bDDnRzF4hEC0QvBcY']
+  provider :twitter, 'caB1N6FvkE0nhaemotOVYg', 'HhDaZpjB5SuXW4CeJPMD72zAh6bDDnRzF4hEC0QvBcY'
 end
